4abcdcb2809b1f87d84d296f4045e597ff0b95d0
dwn <dnielsen@biltmore.com>
2024-08-31 11:38:40 -0400

    top menu animation

--- static/lcars/lcars.js
+++ static/lcars/lcars.js
@@ -10,19 +10,27 @@ $(document).ready(function() {
   // Collapses top menu without animation
   function immediateCollapse() {
     $(".panel-1").css("height", 0);
+    $(".panel-1").hide();
     $(".top-menu").css("height", $(".banner").outerHeight(true) + $(".first-bar-panel").outerHeight());
   }
   // Collapses top menu
   function collapse() {
-    $(".top-menu").stop(true, false).animate({ height: $(".banner").outerHeight(true) + $(".first-bar-panel").outerHeight() });
     $(".panel-1").stop(true, false).animate({ height: 0 }, function() { $(".panel-1").hide(); });
+    $(".top-menu").stop(true, false).animate({ height: $(".banner").outerHeight(true) + $(".first-bar-panel").outerHeight() });
+  }
+  // Expands top menu without animation
+  function immediateExpand() {
+    $(".panel-1").show();
+    const expandedHeight = $(".banner").outerHeight() + $(".data-cascade-button-group").outerHeight();
+    $(".panel-1").css("height", .5 * expandedHeight);
+    $(".top-menu").css("height", expandedHeight);
   }
   // Expands top menu
   function expand() {
     $(".panel-1").show();
     const expandedHeight = $(".banner").outerHeight() + $(".data-cascade-button-group").outerHeight();
     $(".panel-1").stop(true, false).animate({ height: .5 * expandedHeight });
-    $(".top-menu").stop(true, false).animate({ height: $(".banner").outerHeight() + $(".data-cascade-button-group").outerHeight() });
+    $(".top-menu").stop(true, false).animate({ height: expandedHeight });
   }
   // Event expand
   $(".top-menu").on("mouseenter", function() { expand(); });
@@ -30,4 +38,7 @@ $(document).ready(function() {
   $(".top-menu").on("mouseleave touchend", function() { collapse(); });
   // On window resize, immediately collapse top menu
   $(window).resize(function() { immediateCollapse(); });
+  // On window load, immediately collapse top menu unless URL parameter "menu" is present
+  if (window.location.search.indexOf("menu") == -1) immediateCollapse();
+  else immediateExpand();
 });
